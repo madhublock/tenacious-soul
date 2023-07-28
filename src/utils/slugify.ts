9f93aa288eacebb8d635c238ba57bba46a5474c6
Anugu <madhukar.anugu@hrblock.com>
2023-07-27 21:08:46 -0500

    fix slug issue

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,13 +1,16 @@
 import GithubSlugger from 'github-slugger'
 import type { BlogFrontmatter } from "@content/_schemas";
-const slugger = new GithubSlugger()
+
 
 export const slugifyStr = (str: string) => {
+  const slugger = new GithubSlugger()
   return slugger.slug(str);
 }
 
-const slugify = (post: BlogFrontmatter) =>
-  post.postSlug ? slugger.slug(post.postSlug) : slugger.slug(post.title);
+const slugify = (post: BlogFrontmatter) =>{
+  const slugger = new GithubSlugger()
+  return post.postSlug ? slugger.slug(post.postSlug) : slugger.slug(post.title);
+}
 
 export const slugifyAll = (arr: string[]) => arr.map(str => slugifyStr(str));
 
